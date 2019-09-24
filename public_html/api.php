5ef81dd55a5c07c897028da01548a7f624b514af
alashenick <36480214+alashenick@users.noreply.github.com>
2019-09-23 21:55:55 -0400

    Update api.php

--- public_html/api.php
+++ public_html/api.php
@@ -1,7 +1,12 @@
 <?php  
 header('Access-Control-Allow-Origin: *');
 
-
+session_start();
+if (isset($_POST['reset'])){
+//    session_reset();
+//    removeCookie("PHPSESSID");
+    $_SESSION["points"] = 0;
+}
 
     function generate_rack($n){
       $tileBag = "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
@@ -13,14 +18,12 @@ header('Access-Control-Allow-Origin: *');
 
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
-    if(($_POST)) {
+    if(isset($_POST['user-rack'])) {
         $myrack=$_POST['user-rack'];
-        //mysql_query("SQL insert statement.......");
-        }else {
-            $myrack = generate_rack(7);
-        }
-    //$myrack = generate_rack(7);
-    //echo "Random rack: ".$myrack."\n\n";
+    }else {
+        $myrack = generate_rack(7);
+    }
+    
     $racks = [];
     for($i = 0; $i < pow(2, strlen($myrack)); $i++){
         $ans = "";
@@ -36,9 +39,8 @@ header('Access-Control-Allow-Origin: *');
     }
     
     $racks = array_unique($racks);
-    //echo "All sub-racks: ".json_encode(array_values($racks))."\n\n";
     $numracks = sizeof($racks);
-    $words = $bingos = $weights = array();
+    $words = $weights = array();
     
     for ($i=0; $i<$numracks;$i++) {
         if (array_key_exists($i,$racks)) {
@@ -49,7 +51,6 @@ header('Access-Control-Allow-Origin: *');
             $statement = $dbhandle->prepare($query);
             $statement->execute();
             $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-            //print_r($results);
             
             if (count($results) > 0){
                 $str= $results[0]['words'];
@@ -60,40 +61,35 @@ header('Access-Control-Allow-Origin: *');
                     array_push($words,$this_words[$j]);
                     array_push($weights,$this_weight);
                     $numLetters = strlen($this_words[$j]);
-                    if ($numLetters==7) {
-                        array_push($bingos,$this_words[$j]);
-                    //} else if ($numLetters==6) {
-                    //    array_push($sixLetterWords,$this_words[$j]);
-                    //} else if ($numLetters==5) {
-                    //    array_push($fiveLetterWords,$this_words[$j]);
-                    //} else if ($numLetters==4) {
-                    //    array_push($fourLetterWords,$this_words[$j]);
-                    //} else if ($numLetters==3) {
-                    //    array_push($threeLetterWords,$this_words[$j]);
-                    //} else if ($numLetters==2) {
-                    //    array_push($twoLetterWords,$this_words[$j]);
-                    }
                 }
             }
         }
     }
     $results_array = array_combine($words,$weights);
     arsort($results_array);
-    //print_r($results_array);
     
+    $valid_guess = "";
+    $this_guess = "";
+    $points = 0;
     
-    
-    //echo "All words: ".sizeof($words)." total possible words exists.\n". json_encode($words)."\n\n";
-    //echo "Bingos: ".sizeof($bingos)." bingos exist.\n".json_encode($bingos)."\n\n";
-    //echo "Six letter words: ".sizeof($sixLetterWords)." six letter words exist.\n".json_encode($sixLetterWords)."\n\n";
-    //echo "Five letter words: ".sizeof($fiveLetterWords)." five letter words exist.\n".json_encode($fiveLetterWords)."\n\n";
-    //echo "Four letter words: ".sizeof($fourLetterWords)." four letter words exist.\n".json_encode($fourLetterWords)."\n\n";
-    //echo "Three letter words: ".sizeof($threeLetterWords)." three letter words exist.\n".json_encode($threeLetterWords)."\n\n";
-    //echo "Two letter words: ".sizeof($twoLetterWords)." two letter words exist.\n".json_encode($twoLetterWords)."\n\n";
-    
-
+    if(!empty($_POST['user-guesses'])) {
+        $this_guess = $_POST['user-guesses'];
+        for ($i = 0; $i< sizeof($words); $i++) {
+            if ($this_guess==$words[$i]) {
+                $points = $weights[$i];
+                $valid_guess = $this_guess;
+           }
+        }
+    }
+    if (!isset($_SESSION["points"])){
+        $_SESSION["points"] = 0;
+        $_SESSION["recent_points"] = 0;
+    } else {
+        $_SESSION["points"] += $points;
+        $_SESSION["recent_points"] = $points;
+    }
 
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
-    echo json_encode(["rack"=>$myrack,"results"=>$results_array]);
+    echo json_encode(["rack"=>$myrack,"results"=>$results_array, "guess"=>$valid_guess, "total_points"=>$_SESSION["points"], "this_points"=>$_SESSION["recent_points"]]);
 ?>
